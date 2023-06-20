d0d84ae0edccf9fbfda7925416f77d0b07d71d76
roxana.sirbu <roxana.sirbu@student.upt.ro>
2023-06-20 22:14:39 +0300

    add authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,20 @@
 import { NgModule, isDevMode } from '@angular/core';
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-
 import { CommonModule } from '@angular/common';
-
 import {RouterModule} from '@angular/router';
 
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    VerifyEmailComponent
 
   ],
   imports: [
